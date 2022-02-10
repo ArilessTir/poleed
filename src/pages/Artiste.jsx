9d9a46dfad0c9d161e677b4164fd2a2c6fa406c8
ArilessTir <ariless.tir@gmail.com>
2022-02-10 17:29:04 +0100

    replace framer motion by gsap

--- src/pages/Artiste.jsx
+++ src/pages/Artiste.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import gsap from "gsap";
 import { useParams } from "react-router";
 import { BsInstagram, BsSpotify, BsYoutube } from "react-icons/bs";
 import { get_one_artist } from "../services/artistAPI";
@@ -9,6 +10,23 @@ const Artiste = () => {
   useEffect(async () => {
     const data = await get_one_artist(id);
     setArtiste(data);
+
+    gsap.from(".art", {
+      y: 50,
+      delay: 0.3,
+      opacity: 0,
+      stagger: {
+        amount: 0.5
+      }
+    });
+
+    gsap.from(".img", {
+      x: -50,
+      y: -50,
+      delay: 0.3,
+      scale: 1.1,
+      opacity: 0
+    });
   }, []);
 
   return (
@@ -17,7 +35,7 @@ const Artiste = () => {
       md:flex md:min-h-screen md:px-0 mt-20"
     >
       <div
-        className="md:w-1/2 md:h-auto h-96"
+        className="img md:w-1/2 md:h-auto h-96"
         style={{
           backgroundImage: `url(${process.env.REACT_APP_API_URL}${artiste?.attributes.images.data[0].attributes.url})`,
           backgroundRepeat: "no-repeat",
@@ -27,7 +45,7 @@ const Artiste = () => {
       ></div>
 
       <section className="md:w-1/2 md:px-8 md:py-16 max-w-5xl px-4">
-        <div className="md:py-12  py-6">
+        <div className="art md:py-12  py-6">
           <h1 className="font-bold text-4xl uppercase">
             {artiste?.attributes.name}
           </h1>
@@ -36,16 +54,16 @@ const Artiste = () => {
           <h2 className="uppercase">{artiste?.attributes.occupation}</h2>
         </div>
 
-        <div className="text-black leading-7 lg:w-2/3">
+        <div className="art text-black leading-7 lg:w-2/3">
           {artiste?.attributes.description}
         </div>
 
-        <div className="my-6">
+        <div className="art my-6">
           <h1 className="font-bold text-3xl my-5 ">Ses Projets</h1>
           <p> ... </p>
         </div>
 
-        <div className="space-x-5 flex justify-start my-6  ">
+        <div className="art space-x-5 flex justify-start my-6  ">
           <a
             className="cursor-pointer"
             href={artiste?.attributes.instagram}
