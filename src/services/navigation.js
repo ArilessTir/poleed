18ad3b2f7826dcd2674c052115b1b5c8fd9de0ba
ArilessTir <ariless.tir@gmail.com>
2022-03-02 14:48:17 +0100

    add about to navbar

--- src/services/navigation.js
+++ src/services/navigation.js
@@ -1,8 +1,9 @@
 const navlist = [
-  { id: 1, name: "Evenements", direction: "/event" },
+  { id: 1, name: "Au programme", direction: "/event" },
   { id: 2, name: "Contact", direction: "/contact" },
   { id: 3, name: "Projets", direction: "/projects" },
-  { id: 4, name: "Artistes", direction: "/artiste" }
+  { id: 4, name: "Artistes", direction: "/artiste" },
+  { id: 5, name: "A propos", direction: "/" }
 ];
 
 export default navlist;
