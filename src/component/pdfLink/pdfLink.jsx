c1ca7235f69f7698d6faa193a5e259b613476177
ArilessTir <ariless.tir@gmail.com>
2022-03-03 15:52:32 +0100

    add gmaps && change contact

--- src/component/pdfLink/pdfLink.jsx
+++ src/component/pdfLink/pdfLink.jsx
@@ -1,21 +1,19 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { BsFillFilePdfFill } from "react-icons/bs";
-import { saveAs } from "file-saver";
+// import { saveAs } from "file-saver";
 
 const PdfLink = ({ link }) => {
-  const saveFile = () => {
-    saveAs(
-      "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
-      "example.pdf"
-    );
-  };
+  // const saveFile = () => {
+  //   saveAs(
+  //     "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+  //     "example.pdf"
+  //   );
+  // };
   return (
     <div className="flex align items-center cursor-pointer">
       <BsFillFilePdfFill size={40} />
-      <a onClick={saveFile} className="text-xl mt-auto">
-        {link.text}
-      </a>
+      <a className="text-xl mt-auto">{link.text}</a>
     </div>
   );
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -20,7 +20,6 @@ const Contact = () => {
 
   return (
     <body className="max-w-screen-hd mx-auto mt-20">
-      <ContactModal></ContactModal>
       <Notification notif={notif} />
       <h1 className="text-center py-10 font-bold text-4xl">Contact</h1>
       <p className=" max-w-screen-2xl mx-auto text-center px-5">
@@ -33,50 +32,63 @@ const Contact = () => {
         molestiae necessitatibus cupiditate perspiciatis id itaque voluptatum.
       </p>
 
-      <section className="py-4 md:flex items-center justify-center md:space-x-10">
-        <div className=" md:pl-5">
-          <img src={Loca} alt="" />
+      <section className="py-4 md:flex items-center justify-center md:space-x-10 max-w-screen-2xl mx-auto">
+        <div className=" md:pl-5 md:w-1/2">
+          <iframe
+            className="w-full h-80 mx-auto"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10109.936138539986!2d3.1638959697753903!3d50.69240169999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c328ec1eaf6001%3A0xa4bdf7137b03355c!2s3%20Rue%20Nain%2C%2059100%20Roubaix!5e0!3m2!1sfr!2sfr!4v1646296152194!5m2!1sfr!2sfr"
+          ></iframe>
         </div>
 
-        <div className="flex flex-col space-y-4 md:space-y-10 pt-4 px-5 justify-around ">
-          <div className="flex cursor-pointer items-center space-x-2">
+        <div className="flex flex-col space-y-4 md:space-y-10 pt-4 px-5 justify-around md:w-auto">
+          <h1 className="text-3xl"> Ou peux tu nous contacter? </h1>
+          <div className="flex cursor-pointer items-center space-x-2 max-w-xs">
             <FiMail size={23} />
             <a className="text-xl" href="mailto: poleed@gmail.com">
               poleed@gmail.com
             </a>
             <FiCopy
               size={23}
+              style={{
+                marginLeft: "auto"
+              }}
               onClick={() => {
                 handleCopy("poleed@gmail.com");
               }}
             />
           </div>
 
-          <div className="flex cursor-pointer items-center space-x-2">
+          <div className="flex cursor-pointer items-center space-x-2 max-w-xs">
             <FiPhone size={23} />
             <a className="text-xl" href="tel:0767722371">
               0767722371
             </a>
             <FiCopy
               size={23}
+              style={{
+                marginLeft: "auto"
+              }}
               onClick={() => {
                 handleCopy("0767722371");
               }}
             />
           </div>
 
-          <div className="flex cursor-pointer items-center space-x-2">
+          <div className="flex cursor-pointer items-center space-x-2 max-w-xs">
             <FiHome size={23} />
             <a className="text-xl">3 Rue Nain, 59100 Roubaix</a>
             <FiCopy
               size={23}
+              style={{
+                marginLeft: "auto"
+              }}
               onClick={() => {
                 handleCopy("3 Rue Nain, 59100 Roubaix");
               }}
             />
           </div>
 
-          <div className="flex cursor-pointer items-center space-x-2">
+          <div className="flex cursor-pointer items-center space-x-2 max-w-xs">
             <FiInstagram size={23} />
 
             <a
@@ -89,6 +101,9 @@ const Contact = () => {
             </a>
             <FiCopy
               size={23}
+              style={{
+                marginLeft: "auto"
+              }}
               onClick={() => {
                 handleCopy("@Poleed");
               }}
